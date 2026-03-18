251a047f691464c898cf492cb7a7d02cd1cf297e
Alainaa10 <alainasingh@gmail.com>
2026-03-18 09:31:35 +0530

    [Alaina] Added UC1 feature

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to Book My Stay App");
+
+        // Display application details
+        System.out.println("Hotel Booking System v1.0");
+
+        // Inform user that application started successfully
+        System.out.println("Application started successfully.");
+
+        // End message
+        System.out.println("Thank you for using Book My Stay!");
+    }
+}
